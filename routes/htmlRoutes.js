ca57b63a43eb6df433e5fc0fd40d6d103e6636b2
Rhowen01 <rraju002@ucr.edu>
2022-08-20 10:12:44 -0700

    routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,16 +4,16 @@ const router = require('express').Router();
 
 // This has to be on the top than the other routers
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 // If no matching route is found default to home page
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 
