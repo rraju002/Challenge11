85b5ec67396f475c87c485217d44a7d0860673d6
Rhowen01 <rraju002@ucr.edu>
2022-08-25 12:32:54 -0700

    inline server.js

--- server.js
+++ server.js
@@ -23,6 +23,44 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 // Listener
+router.get('/notes', function (req, res) {
+    saveData
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+// POST request
+router.post('/notes', (req, res) => {
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+// Bonus - DELETE request
+router.delete('/notes/:id', function (req, res) {
+    saveData
+        .deleteNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
+});
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// If no matching route is found default to home page
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`API server is ready on port ${PORT}!`);
 });
